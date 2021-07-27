b09fdfdf0ca01417d798ff24e07efc37ac333e7c
Div711 <87711647+Div711@users.noreply.github.com>
2021-07-27 13:31:25 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,5 +7,10 @@ def begin():
     print("Welcome to Password Generator login")
     option = input("login or registor (login,reg): ")
     if(option!="login" and option!="reg"):
-        begin()
-begin()
+        option = begin()
+    return option
+opt = begin()
+if(opt=="login"):
+    name = input("Enter your name: ")
+    password = input("Enter your password: ")
+    login(name,password)
